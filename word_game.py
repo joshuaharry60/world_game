b9a828c969941524aaff522f07607e3470044f89
Joshua Harry <joshuaharry60@gmail.com>
2026-05-29 15:57:18 +0100

    first commit

---
+++ word_game.py
@@ -0,0 +1,35 @@
+#WORD GAME IN PYTHON:
+"""
+
+adjective1 = input("enter an adjective (description): ")
+noun1 = input("Enter a noun (a name of a person,place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"To day I went to a {adjective1} zoo. ")
+print(f"In an exhibit, I saw a  {noun1}")
+print(F"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+""
+
+name1 = input("Enter your name: ")
+noun2= input("Enter a place {noun2}: ")
+adjective = input("Enter an adjective (description): ")
+verb = input("Enter a verb ending with 'ing': ")
+
+
+print(f"one day, {name1} went to {noun2}.")
+print(f"and felt very {adjective}. while {verb}")
+"""
+
+
+
+noun = input("Enter your name: ")
+noun2 = input("Enter name of a place {noun}:")
+adjective = input("Descript what you saw {adjective}:")
+verb = input("Enter a verb ending with 'ing': ")
+
+print(f"One faithful morning  {noun} went with his entire family member to attend mornig mass ")
+print(f"On getting to {noun2} his see where amaze with what he saw, likewise his family members within the church premise. thougth its was  very {adjective} experience  encountered. ")
+print(f"But, none the less God was in control and the situation was calm and arrested by other members present in church to worship. but indeed was a great experience though very {verb}")
